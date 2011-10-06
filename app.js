957fd3b9c8724413a3cdb75850f31e2679e5b267
Dan Gebhardt <dan@cerebris.com>
2011-10-06 16:18:04 -0400

    1.0b1 works with domcache = true and removing prev pages on pageshow

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).bind("mobileinit", function() {
+  $.mobile.page.prototype.options.domCache = true;
+});
+
+$('div').live('pageshow',function(event, ui) {
+  if (ui.prevPage) ui.prevPage.remove();
+});
+
+$(document).bind('pagecreate',function(event, ui) {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  if (minutes < 10) minutes = "0" + minutes;
+  var seconds = now.getSeconds();
+  if (seconds < 10) seconds = "0" + seconds;
+  
+  var page = $(event.target);
+  page.find('.load-time').text(hours + ":" + minutes + ":" + seconds);
+});
+
